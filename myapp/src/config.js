da2f8c287d4ba10988625aad82ed1b5822adb2f1
Dearkano <vaynetian@cc98.org>
2018-11-05 20:49:46 +0800

    Update myapp/src/config.js

--- myapp/src/config.js
+++ myapp/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     chain: 'http://121.196.200.225:1337',
     privateKey: '233339F7E831297B7E6173D1FAAD4C74E95C992653F2F26A870D95758B72C3F1',
-    contractAddress: '0xb40A639137B72628E13cF41505e334f3AFeBaDc4'
+    contractAddress: '0xA521aACba52a836ADFEad2e5e3cc6809C7a496e5'
   }
   module.exports = config
   
